55e223c17286cf4816eb92e9530426590810cc24
gyogo <48716673+gyogo@users.noreply.github.com>
2020-07-16 10:12:14 +0900

    first

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Administrator\PycharmProjects\NetworkProgramming\Honest\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
